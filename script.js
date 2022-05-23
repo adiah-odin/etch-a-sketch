da750cd19fe6dcf180828ef7a58353690c9fa19d
nathanieladiah <nathanieladiah@gmail.com>
2022-05-23 07:14:17 -0400

    Prompt for board size and use that for # of grids

--- script.js
+++ script.js
@@ -2,29 +2,68 @@
 const board = document.getElementById('board');
 
 // This will be changed dynamically
-const boardSize = 16; 
+let boardSize = 16; 
+const clearBtn = document.getElementById('clear');
+const sizeBtn = document.getElementById('size');
 
-createBoard();
+createBoard(boardSize);
 
-function createBoard() {
+function createBoard(size) {
+	// remove any child elements of board first
+	while (board.firstChild) {
+		board.removeChild(board.firstChild);
+	}
 	// Create a square sized by the boardSize variable
-	for (let i = 0; i < boardSize; i++) {
-		for (let j = 0; j < boardSize; j++) {
+	for (let i = 0; i < size; i++) {
+		for (let j = 0; j < size; j++) {
+			// set the custom property of the board size
+			board.style.setProperty('--board-size', size);
+
 			// create div element and append to board
 			const square = document.createElement('div');
 			square.classList.add('square')
 			square.id = `square-${i},${j}`
 			square.addEventListener('mouseenter', () => {
-				firstPass(event)
-			})
+				switchColor(event)
+			});
 			board.append(square);
 		}
 	}
 }
 
 
-function firstPass(e) {
+function switchColor(e) {
 	// console.log(e.target)
 	// console.log('mousePassing');
-	e.target.classList.add('pass1');
+	if (!e.target.classList.contains('pass1')) {
+		e.target.classList.add('pass1');
+	} else if (!e.target.classList.contains('pass2')) {
+		e.target.classList.add('pass2');
+	}
 }
+
+function resetColors() {
+	// a regular expression to match all occurrences of pass 
+	var classRegx = new RegExp('pass' + '[^ ]*[ ]?', 'g');
+	const squares = document.querySelectorAll('.square');
+	squares.forEach(square => {
+		square.className = square.className.replace(classRegx, '');
+	})
+}
+
+clearBtn.onclick = resetColors;
+
+
+function resize() {
+	let boardSize = parseInt(prompt("Enter a board size (max 100)"));
+
+	if (typeof(boardSize) !== 'number' || boardSize < 16 || boardSize > 100) {
+		alert("Please enter an integer between 16 and 100");
+		return;
+	}
+
+	createBoard(boardSize);
+
+}
+
+sizeBtn.onclick = resize;
